cf7520d3164f01f5d0c2f46d9a294237d51e765f
cowboy8625 <cowboy8625@yahoo.com>
2025-01-11 08:14:03 -0600

    ftplugin(c): update make command

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -2,5 +2,5 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 0
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.cmd [[ set makeprg=gcc\ %\ -o\ %:r]]
+vim.cmd [[ set makeprg=make]]
 vim.keymap.set('n', ';;', ':make<CR>')
